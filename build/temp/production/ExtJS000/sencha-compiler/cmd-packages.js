d47e671e96a894106fc85e3df9df8973141d44a5
Lee Boonstra <lee.boonstra@sencha.com>
2013-10-14 19:53:19 +0200

    theme build

--- build/temp/production/ExtJS000/sencha-compiler/cmd-packages.js
+++ build/temp/production/ExtJS000/sencha-compiler/cmd-packages.js
@@ -1,3 +1,4 @@
+Ext.setVersion("Nouveau", "2.0.0");
 Ext.setVersion("ext-theme-base", "4.2.1");
-Ext.setVersion("ext-theme-classic", "4.2.1");
+Ext.setVersion("ext-theme-neptune", "4.2.1");
 Ext.setVersion("ext-theme-neutral", "4.2.1");
